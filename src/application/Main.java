b8b3a145dac4828aa6d66aaf5c5c7d18962b11dc
Michael Deng <mdeng123@gmail.com>
2014-09-12 13:33:27 -0400

    made changes to Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,29 +1,61 @@
 package application;
-	
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 
 
 public class Main extends Application {
-	
+
 	public static final Integer STAGE_WIDTH = 1100;
 	public static final Integer STAGE_HEIGHT = 700;
 	
+	private ApplicationLoop myGame;
+
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,STAGE_WIDTH,STAGE_HEIGHT);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		Pane root = new Pane();
+		Scene scene = new Scene(root,STAGE_WIDTH,STAGE_HEIGHT);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		populateStage(primaryStage, scene);
+	}
+
+	/**
+	 * Creates and displays the game's main scene. Runs the game loop.
+	 */
+	private void playGame(Stage stage) {
+		Scene scene = myGame.init(stage, 750, 750);
+		populateStage(stage, scene);
+		runGameLoop();
+	}
+
+	/**
+	 * Runs the game loop and the animation that goes along with the game loop.
+	 */
+	public void runGameLoop() {
+		KeyFrame frame = myGame.start();
+		Timeline animation = new Timeline();
+		animation.setCycleCount(Timeline.INDEFINITE);
+		animation.getKeyFrames().add(frame);
+		animation.play();
 	}
 	
+	/**
+	 * Populates and shows the stage.
+	 * 
+	 * @param scene
+	 *            : The application's current scene.
+	 */
+	private void populateStage(Stage stage, Scene scene) {
+		stage.setTitle("Cookie Fall!");
+		stage.setScene(scene);
+		stage.show();
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
