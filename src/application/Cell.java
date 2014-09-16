9205862a7a27065bd9e17d7d20625932c59990bb
David Zhang <David@davids-mbp-110.wireless.duke.local>
2014-09-15 23:44:04 -0400

    Set up variables in Cell. Started Segregation.

--- src/application/Cell.java
+++ src/application/Cell.java
@@ -1,5 +1,12 @@
 package application;
 
-public abstract class Cell {
+import javafx.scene.paint.Paint;
 
+public abstract class Cell {
+	
+	int xPos;
+	int yPos;
+	Paint currentState;
+	Paint updatedState;
+	Cell[][] Matrix;
 }
