39c84cb269863fc0c58a1427c4982354e6f04a91
Pranava Raparla <pranava.raparla@gmail.com>
2014-09-18 00:54:42 -0400

    Cosmetic

--- src/application/Segregation.java
+++ src/application/Segregation.java
@@ -7,8 +7,7 @@ public class Segregation extends Cell {
 
 	private int same;
 	private int different;
-	private int threshold;
-
+	private double threshold;
 
 	public Segregation(){
 		same = 0;
@@ -26,7 +25,6 @@ public class Segregation extends Cell {
 		threshold = num;
 	}
 
-
 	private double percentageCalc(){
 		for(int x=0; x<ApplicationConstants.NUM_OF_COLUMNS; x++){
 			for(int y=0; y<ApplicationConstants.NUM_OF_ROWS; y++){
@@ -35,17 +33,13 @@ public class Segregation extends Cell {
 
 				for(int i=x-1; i<=x+1; i++){
 					for(int j= y-1; j<=y+1; j++){
-
 						if(Matrix[i][j].currentState==Matrix[x][y].currentState){
 							same++;
 						}	
-
 						if(Matrix[i][j].currentState!=null){
 							if(Matrix[i][j].currentState!=Matrix[x][y].currentState){
 								different++;								
 							}
-
-
 						}							
 					}
 					same = same -1;
@@ -55,26 +49,21 @@ public class Segregation extends Cell {
 		return same/(same+different);
 	}
 
-
 	private void cellMover(double percentage){
 		if(percentage < threshold){
 			for(int b = yPos; b<ApplicationConstants.NUM_OF_ROWS; b++){
 				for(int a = 0; a<ApplicationConstants.NUM_OF_COLUMNS; a++){
 					if(b==yPos && a<=xPos) // check if in part of row before current cell
 						continue; // continue to next iteration if true
-
 					if (Matrix[a][b].currentState==Color.WHITE && Matrix[a][b].updatedState == null){
 						Matrix[a][b].updatedState = Matrix[xPos][yPos].currentState;
 						Matrix[xPos][yPos].updatedState = Color.WHITE;
-
-
 					}
 				}
 			}
 		}
 		Matrix[xPos][yPos].updatedState = Matrix[xPos][yPos].currentState;		
 	}
-
 
 	@Override
 	void setCurrentState(String s) {
@@ -95,10 +84,6 @@ public class Segregation extends Cell {
 	@Override
 	public void updateCell(int i, int j, Cell[][] cellMatrix) {
 		// TODO Auto-generated method stub
-
 	}
-
-
-
-	}
+}
 
