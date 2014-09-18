1cb6c1d350118c4efcfc416e5de1e74d82ad2631
Pranava Raparla <pranava.raparla@gmail.com>
2014-09-17 21:39:54 -0400

    Added basic XML Reader with sample template for Segregation

---
+++ src/application/CellXMLReader.java
@@ -0,0 +1,62 @@
+package application;
+
+import java.io.IOException;
+import java.util.*;
+
+import javax.xml.parsers.*;
+
+import javafx.scene.paint.*;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+public class CellXMLReader {
+	
+	public Document getAndLoadXMLFile(String filename) throws ParserConfigurationException, IOException, SAXException {
+		// Get DOM builder from DOM builder factory
+		DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+		//Load and parse XML document, which is now stored as a Tree
+		Document document = (Document) builder.parse(ClassLoader.getSystemResourceAsStream(filename));
+		return document;
+	}
+	
+	public NodeList getNodeListFromDocument(Document document) {
+		List<Cell> cellList = new ArrayList<>();
+		
+		NodeList nodeList = document.getDocumentElement().getChildNodes();
+		
+		for(int i=0; i<nodeList.getLength();i++) {
+			Node node = nodeList.item(i);
+			if(node instanceof Element) {
+				Cell cell;
+				if(node instanceof Segregation) {
+					cell = new Segregation();
+				}
+				NodeList childNodes = node.getChildNodes();
+				for(int j=0; j<childNodes.getLength(); j++) {
+					Node cNode = childNodes.item(j);
+					
+					//Identifying child tag of cell encountered
+					if(cNode instanceof Element) {
+						String content = cNode.getLastChild().getTextContent().trim();
+						switch(cNode.getNodeName()) {
+							case "xPos":
+								cell.setXPos(Integer.parseInt(content));
+								break;
+							case "yPos":
+								cell.setYPos(Integer.parseInt(content));
+								break;
+							case "state":
+								cell.setCurrentState(content);
+								break;
+						}
+					}
+				}
+				cellList.add(cell);
+			}
+				
+		}
+		
+		return nodeList;
+	}
+}
